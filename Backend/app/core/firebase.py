1bef9cc24611b62f26ab7a7b389722c8525a2960
Sneha Gaikwad <padolswati85@gmail.com>
2026-07-07 23:20:15 +0530

    Frontend ready for deployment

--- Backend/app/core/firebase.py
+++ Backend/app/core/firebase.py
@@ -1,9 +1,17 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("/etc/secrets/firebase-service-account.json")
+if not firebase_admin._apps:
+    firebase_config = os.environ.get("FIREBASE_CREDENTIALS")
 
-firebase_admin.initialize_app(cred)
+    if not firebase_config:
+        raise Exception("FIREBASE_CREDENTIALS environment variable not found.")
+
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
 
 def verify_firebase_token(token: str):
     return auth.verify_id_token(token)
