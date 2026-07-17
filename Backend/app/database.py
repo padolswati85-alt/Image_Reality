b9a934c7c3f9c3450734d241c1297f94a339b751
Sneha Gaikwad <padolswati85@gmail.com>
2026-07-17 17:21:48 +0530

    Fix Aiven SSL database connection

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,49 +1,48 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Load .env file
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 load_dotenv()
 
-# Environment variables
+
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-# MySQL Database URL
+
 DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
-# SQLAlchemy Engine
+
 engine = create_engine(
     DATABASE_URL,
-    echo=False,
-    pool_pre_ping=True,
-    pool_recycle=3600,
     connect_args={
         "ssl": {
-            "ssl_mode": "REQUIRED"
+            "ca": "ca.pem"
         }
     },
+    pool_pre_ping=True,
+    pool_recycle=300,
+    echo=True
 )
 
-# Session
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine,
+    bind=engine
 )
 
-# Base Model
+
 Base = declarative_base()
 
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
